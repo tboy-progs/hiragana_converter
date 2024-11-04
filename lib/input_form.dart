4e236b3eebf87fe1dbecd0291f6f2e81cfbd03e4
tboy <gluestick829@gmail.com>
2024-11-04 23:52:27 +0900

    add:入力した文字列を取得

--- lib/input_form.dart
+++ lib/input_form.dart
@@ -9,6 +9,7 @@ class InputForm extends StatefulWidget {
 
 class _InputFormState extends State<InputForm> {
   final _formKey = GlobalKey<FormState>();
+  final _textEditingController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -20,6 +21,7 @@ class _InputFormState extends State<InputForm> {
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 16),
             child: TextFormField(
+              controller: _textEditingController,
               maxLines: 5,
               decoration: const InputDecoration(
                 hintText: '文章を入力してください',
@@ -36,12 +38,21 @@ class _InputFormState extends State<InputForm> {
           ElevatedButton(
             onPressed: () {
               final formState = _formKey.currentState!;
-              formState.validate();
+              if (!formState.validate()) {
+                return;
+              }
+              debugPrint('text = ${_textEditingController.text}');
             },
             child: const Text('変換'),
           ),
         ],
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _textEditingController.dispose();
+    super.dispose();
   }
 }
